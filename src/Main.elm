4125f1867a527245acc1212858fec3b0bfc8f5b1
Joe Lea <joeleapublic@gmail.com>
2017-10-28 00:05:54 +0100

    Can draw

--- src/Main.elm
+++ src/Main.elm
@@ -15,24 +15,44 @@ import Html.Attributes exposing (class, placeholder)
 import Collage exposing (..)
 import Element exposing (..)
 
+import Mouse
+import Window
+import Task
+
 ---- MODEL ----
 
 dotSize = 30
 dotGap = 5
 scaleFactor = dotGap + (2 * dotSize)
 gridSize = 4
+gridPixels = (2 * (gridSize + 1) ) * scaleFactor
+
+topSpacing = 200
 
 type alias Model =
     { min: Maybe Int
     , max: Maybe Int
     , shuffledPositions: List Position
     , value: Int
+    , paths: List Path
+    , currentPath: List (Float, Float)
+    , screenWidth: Int
     }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { min = Nothing, max = Nothing, shuffledPositions = possiblePositions, value = 0 }, Cmd.none )
+    (
+        { min = Nothing
+        , max = Nothing
+        , shuffledPositions = possiblePositions
+        , value = 0
+        , paths = []
+        , currentPath = []
+        , screenWidth = 0
+        }
+    , Task.perform SetWindowWidth Window.width
+    )
 
 ---- UPDATE ----
 
@@ -43,6 +63,8 @@ type Msg
     | ChangeValue (Maybe Int)
     | ShuffledPositions (List Position)
     | Shuffle
+    | MouseMove Mouse.Position
+    | SetWindowWidth Int
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -81,6 +103,22 @@ update msg model =
             ( { model | shuffledPositions = positions }
             , Cmd.none
             )
+
+        MouseMove position ->
+            let
+                originalPos = Position position.x -position.y
+                translation = Position -(model.screenWidth // 2) (topSpacing + ( gridPixels // 2) )
+                posOnGrid = translatePosition translation originalPos
+            in
+                ( { model | currentPath = model.currentPath ++ [( toFloat posOnGrid.x, toFloat posOnGrid.y )] }
+                , Cmd.none
+                )
+        SetWindowWidth width ->
+            ( { model | screenWidth = width }
+            , Cmd.none
+            )
+
+
         default ->
             ( model, Cmd.none )
 
@@ -90,6 +128,7 @@ view : Model -> Html Msg
 view model =
     let
         positions = List.take model.value model.shuffledPositions
+        paths = path (model.currentPath) :: model.paths
     in
         div [ class "app" ]
             [
@@ -104,7 +143,7 @@ view model =
                     [ button [ class "shuffle-button", onClick Shuffle ] [ Html.text "shuffle" ]
                     ]
                 ]
-            , div [ class "dots" ] <| [ partition positions ]
+            , div [ class "dots" ] <| [ partition positions paths ]
             ]
 
 
@@ -133,17 +172,26 @@ type alias Position =
     , y: Int
     }
 
+translatePosition : Position -> Position -> Position
+translatePosition translation position = Position (position.x + translation.x) (position.y + translation.y)
+
 translate : Position -> Form -> Form
 translate position form = move (toFloat position.x, toFloat position.y) form
 
-partition : List Position -> Html Msg
-partition positions =
+partition : List Position -> List Path -> Html Msg
+partition dotPositions paths =
     let
-        elements = List.map dot positions
-        size = (2 * (gridSize + 1) ) * scaleFactor
-        dots = collage size size elements
+        linestyle = { defaultLine | join = Smooth, width = 100.0 }
+        lines = List.map (traced defaultLine) paths
+        dots = List.map dot dotPositions
+        allElements = collage gridPixels gridPixels <| dots ++ lines
     in
-        toHtml dots
+        toHtml allElements
+
+subscriptions model = Sub.batch
+    [ Mouse.moves MouseMove
+    , Window.resizes (\{width, height} -> SetWindowWidth width)
+    ]
 
 ---- PROGRAM ----
 
@@ -153,5 +201,5 @@ main =
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
